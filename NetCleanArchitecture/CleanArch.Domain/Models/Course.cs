f2a39a0406fbc10e972d82197dcadee24baf7d06
osolisd <oscarasolisd@gmail.com>
2025-03-23 18:21:49 -0500

    Creación del proyecto Domain y creación de nueva clase dominio oCourse

---
+++ NetCleanArchitecture/CleanArch.Domain/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String ImageUrl { get; set; }
+    }
+}
