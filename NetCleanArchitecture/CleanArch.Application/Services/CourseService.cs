ff3b41ebda37fc4294194d60c8dada87361888ca
osolisd <oscarasolisd@gmail.com>
2025-03-24 18:56:51 -0500

    Implementación del método GetCourses en el repositorio y en el servicio

---
+++ NetCleanArchitecture/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,29 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private ICourseRepository _courseRepository;
+
+        public CourseService(ICourseRepository _courseRepository)
+        {
+            this._courseRepository = _courseRepository;
+        }
+
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel()
+            {
+                Courses = this._courseRepository.GetCourses()
+            };
+        }
+    }
+}
