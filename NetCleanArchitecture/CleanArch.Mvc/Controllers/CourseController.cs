9f0fa4a806b397789d45a08343cecec3705c8a34
osolisd <oscarasolisd@gmail.com>
2025-03-25 22:40:00 -0500

    Creación del controlador Course y creación del proyecto IoC

---
+++ NetCleanArchitecture/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            CourseViewModel courseViewModel = this.courseService.GetCourses();
+            return View(courseViewModel);
+        }
+    }
+}
